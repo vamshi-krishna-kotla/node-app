db8a82b3ae4ca01ff60dfeeb6542750fc37778c6
vamshi-krishna-kotla <vkotla@webmd.net>
2020-07-19 12:51:57 +0530

    Added GET request for routes

--- GUI/lib/server.js
+++ GUI/lib/server.js
@@ -32,13 +32,47 @@ server.httpServer.get('/', function (req, res) {
 	});
 });
 
+// GET method handler for routes
+server.httpServer.get('/:route', function (req, res) {
+	// return the page template if the route is available; else redirect to error page
+	if(server.availableRoutes.includes(req.params.route)) {
+		// find the requested page as per the route
+		helpers.findPage(req.params.route, function(err, data) {
+			if(!err && data) {
+				// send the page template
+				res.status(200);
+				res.setHeader('Content-Type', 'text/html');
+				res.end(data);
+			}
+			else {
+				// if page not found then call for an error
+				res.status(500);
+				console.log(err);
+				res.end('Error fetching template');
+			}
+		});
+	}
+	else {
+		// redirect to error page since the route is not available
+		res.status(404);
+		res.setHeader('Content-type', 'text/html');
+		/**
+		 * @TODO create an error page and redirect to it
+		 */
+		res.end('<h1>404 Page not found</h1>');
+	}
+});
+
+// currently available routes for the GUI
+server.availableRoutes = ['sign-in'];
+
 /**
  * Initialise the server
  */
 server.init = function () {
 	// start the server and listen
 	server.httpServer.listen(4000, function () {
-		console.log('GUI serving at port: 4000');
+		console.log('GUI serving at: \x1b[35m%s\x1b[0m', 'http://localhost:4000');
 	});
 };
 
