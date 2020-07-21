f98c7217ea0780b196d556ace09d6de6003ad89d
vamshi-krishna-kotla <vkotla@webmd.net>
2020-07-21 21:38:46 +0530

    Added MiniCssExtractPlugin to split build CSS into separate files and loading them onto pages and moved webpack to devDependencies

--- GUI/webpack.config.js
+++ GUI/webpack.config.js
@@ -1,29 +1,46 @@
 const path = require('path');
 const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const scriptsPath = path.resolve(__dirname, 'src', 'scripts');
 const scripts = fs.readdirSync( scriptsPath, "utf8" );
 
 var fileMap = {};
 scripts.forEach(script => {
-	fileMap[script] = path.resolve(scriptsPath, script);
+	fileMap[script.replace(/.js$/, '')] = path.resolve(scriptsPath, script);
 });
 
 module.exports = {
 	entry: fileMap,
 	output: {
-		path: path.resolve(__dirname, 'dist', 'scripts'),
-		filename: '[name]'
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'scripts/[name].js'
 	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'styles/[name].css'
+		})
+	],
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				loader: ['style-loader', 'css-loader']
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader'
+				]
 			},
 			{
 				test: /\.(scss|sass)$/,
-				loader: ['style-loader', 'css-loader', 'sass-loader']
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					'css-loader',
+					'sass-loader'
+				]
 			}
 		]
 	}
