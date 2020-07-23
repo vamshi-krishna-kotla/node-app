319918b057402f0753b7a4b284e81e605b7540c8
vamshi-krishna-kotla <vkotla@webmd.net>
2020-07-23 18:03:23 +0530

    Added github and social-media icons to footer

--- GUI/src/scripts/global_images.js
+++ GUI/src/scripts/global_images.js
@@ -3,8 +3,12 @@
  * it is no where imported but the build from this file is generating images to be served
  */
 
+ /**
+  * @todo find a way to dynamically import the images instead of giving static paths
+  */
 import '../images/babel_logo.png';
 import '../images/expressjs_logo.png';
 import '../images/node_logo.png';
 import '../images/sass_logo.png';
 import '../images/webpack_logo.png';
+import '../images/github.png';
