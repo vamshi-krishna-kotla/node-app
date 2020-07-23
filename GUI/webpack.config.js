a90bb9cddfa0ac347ec9c285e7ddecfcd1568fdf
vamshi-krishna-kotla <vkotla@webmd.net>
2020-07-23 15:48:42 +0530

    Passing webpack mode from terminal instead of using an env variable [FYI: NODE_ENV varriable is being set based on the mode already]

--- GUI/webpack.config.js
+++ GUI/webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const env = (process.env.ENV || 'none').trim();
-
 const scriptsPath = path.resolve(__dirname, 'src', 'scripts');
 const scripts = fs.readdirSync( scriptsPath, "utf8" );
 
@@ -14,7 +12,6 @@ scripts.forEach(script => {
 
 module.exports = {
 	entry: fileMap,
-	mode: env,
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'scripts/[name].js'
