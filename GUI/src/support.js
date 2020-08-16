096216fa378279afbf1fd32b0c79a1a28ba8e681
vamshi-krishna-kotla <vkotla@webmd.net>
2020-08-16 15:19:15 +0530

    Modified Alert class functionality

--- GUI/src/support.js
+++ GUI/src/support.js
@@ -1,26 +1,56 @@
 export class Alert {
-	constructor(displayText, linkText, link) {
-		this.displayText = displayText || '';
-		this.linkText = linkText || false;
-		this.link = link || false;
+	constructor(data) {
+		this.displayText = data.message || '';
+		this.linkText = data.linkText || false;
+		this.link = data.link || false;
+		this.type = (['notify', 'success', 'fail'].indexOf(data.type) > -1) ? data.type : 'notify';
 	}
 
-	appendToDOM(targetSelector) {
+	appendAlertToDOM(targetSelector) {
 		// remove previous alerts
-		this.removePreviousAlert();
+		this.removeAlert();
+		
+		// build new alert
+		var newAlertDiv = this.buildAlertHTML();
 		
 		// append new alert to DOM
-		var newDiv = document.createElement('div');
-		newDiv.setAttribute('class', 'alert');
-		newDiv.innerHTML = this.displayText;
-		document.querySelector(targetSelector).appendChild(newDiv);
+		document.querySelector(targetSelector).appendChild(newAlertDiv);
 	}
 
-	removePreviousAlert() {
+	removeAlert() {
 		// if there is any previous alert then take it off of DOM
 		var prevAlert = document.querySelector('.alert');
 		if(prevAlert) {
 			prevAlert.remove();
 		}
+	}
+
+	buildAlertHTML() {
+		// build the alert with relevant classes
+		var newDiv = document.createElement('div');
+		newDiv.setAttribute('class', 'alert');
+		newDiv.classList.add(`alert-${this.type}`);
+		var text = document.createElement('span');
+		text.appendChild(document.createTextNode(this.displayText))
+		newDiv.appendChild(text);
+
+		// if any link is given, append it
+		if(this.link && this.linkText) {
+			var appendLink = document.createElement('a');
+			appendLink.setAttribute('href', `/${this.link}`);
+			appendLink.appendChild(document.createTextNode(this.linkText));
+			newDiv.appendChild(appendLink);
+		}
+		
+		// add close icon for the alert box
+		var closeAlert = document.createElement('span');
+		closeAlert.innerHTML = '&#x2715;';
+		closeAlert.setAttribute('class', 'alert-close');
+		closeAlert.onclick = () => {
+			this.removeAlert();
+		};
+		newDiv.appendChild(closeAlert);
+
+		return newDiv;
 	}
 }
