e802a4b237a4537eada4134b21970fe73e99e43b
vamshi-krishna-kotla <vkotla@webmd.net>
2020-08-16 16:57:03 +0530

    Added basic sign-in page

--- GUI/src/scripts/sign-in.js
+++ GUI/src/scripts/sign-in.js
@@ -1,7 +1,84 @@
 import '../styles/sign-in.scss';
 
-import { hideHeaderLinks } from '../support';
+import { Alert,hideHeaderLinks } from '../support';
 
 window.onload = function () {
 	hideHeaderLinks('create-check', 'sign-in', 'log-out');
+
+	document.querySelector('#login-form').addEventListener('submit', function(event) {
+		// stop the form from default submission
+		event.preventDefault();
+		let payload = {};
+		let elements = [...this.elements];
+		['phone', 'password'].forEach(sel => {
+			payload[sel] = elements.find(function (e) {
+				return e.className == sel;
+			}).value;
+		});
+
+		signInUser(payload);
+
+	});
 };
+
+async function signInUser(payload) {
+	var alertData = {
+		'message': '',
+		'linkText': false,
+		'link': false,
+		'type': 'notify'
+	};
+	try {
+		// POST to API with payload
+		var response = await fetch('http://localhost:3000/tokens', {
+			method: 'POST',
+			body: JSON.stringify(payload)
+		});
+
+		var data = await response.json();
+
+		// handle the response
+		if(response.status == 200) {
+			// if the POST request returns 200 - display a success message to the user
+			// give a link to dashboard for the signed in user - take to dashboard page
+			alertData = {
+				'message': 'Signed In successfully! Session expires in 1 hour!!',
+				'linkText': '<link to dashborad>!',
+				'link': 'dashboard',
+				'type': 'success'
+			};
+
+			// handle the response from the succesful request
+			handleResponse(data);
+		}
+		else if(response.status == 400 || response.status == 500) {
+			// if the response is invalid - display an error message to the user with msg regarding invalid request
+			alertData = {
+				'message': data.Error,
+				'type': 'fail'
+			};
+		}
+		else {
+			// if the response is anything else - display an error message to the user
+			throw 'Error while signing in! Please try again later!';
+		}
+	}
+	catch(e) {
+		alertData = {
+			'message': (typeof e === 'string') ? e : 'Error: Couldn\'t make a successful HTTP fetch call!',
+			'type': 'notify'
+		};
+	}
+	finally {
+		const a = new Alert(alertData);
+		a.appendAlertToDOM('.page-content');
+	}
+}
+
+function handleResponse(responseData) {
+	
+	console.log(responseData.expires - new Date());
+	/**
+	 * @todo handle the data to set required variables for a signed in user
+	 */
+}
