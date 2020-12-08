24b1d50c939faa8c809f11ef98718ff831053485
vamshi-krishna-kotla <vkotla@webmd.net>
2020-12-08 12:39:32 +0530

    Added babel support for JS files

--- GUI/webpack.config.js
+++ GUI/webpack.config.js
@@ -29,6 +29,16 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
 				test: /\.css$/,
 				use: [
 					{
