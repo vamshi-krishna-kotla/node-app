fb5f480fa135307d478bede26f73e121a8d0fb19
vamshi-krishna-kotla <vkotla@webmd.net>
2021-04-27 19:30:09 +0530

    Set preset to parse react components and JSX

--- GUI/webpack.config.js
+++ GUI/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-env']
+						presets: ['@babel/preset-env', '@babel/preset-react']
 					}
 				}
 			},
